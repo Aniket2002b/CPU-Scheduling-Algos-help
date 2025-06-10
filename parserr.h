795f2941d8e694988c7f6a33ed9647b425a1c55a
Aniket2002b <129680958+Aniket2002b@users.noreply.github.com>
2025-06-10 19:36:07 +0530

    Making a commit

--- parserr.h
+++ parserr.h
@@ -4,7 +4,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 /**This file handles \\\parsing the data we are going to work with bn
-* and holds all the global variables we parse into//
+* and holds all the global variables we parse into
 */
 
 string operation;
